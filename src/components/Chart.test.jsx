4d1a57bad8b5265dc75f829ed21939a85fd60d8d
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-09-25 11:45:58 +0100

    format test code

--- src/components/Chart.test.jsx
+++ src/components/Chart.test.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Chart from './Chart';
+import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import Chart from "./Chart";
 
-describe('Chart component', () => {
-    it("renders the file input", () => {
-        render(<Chart />);
-        const input = screen.getByTestId("file-input");
-        expect(input).toBeInTheDocument();
-    })
-    it("renders the chart when file is uploaded", () => {
-        render(<Chart />);
-        const input = screen.getByTestId("file-input");
-        fireEvent.change(input, {
-            target: {
-                files: [new File([""], "test.csv", { type: "text/csv" })],
-            },
-        });
-        waitFor(() => {
-            const chart = screen.getByTestId("chart");
-            expect(chart).toBeInTheDocument();
-        })
-    })
-})
+describe("Chart component", () => {
+  it("renders the file input", () => {
+    render(<Chart />);
+    const input = screen.getByTestId("file-input");
+    expect(input).toBeInTheDocument();
+  });
+  it("renders the chart when file is uploaded", () => {
+    render(<Chart />);
+    const input = screen.getByTestId("file-input");
+    fireEvent.change(input, {
+      target: {
+        files: [new File([""], "test.csv", { type: "text/csv" })],
+      },
+    });
+    waitFor(() => {
+      const chart = screen.getByTestId("chart");
+      expect(chart).toBeInTheDocument();
+    });
+  });
+});
