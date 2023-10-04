e85241343ea3d78e0f40cdcf0c0894e765d156d6
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-10-04 13:39:16 +0100

    change point config to array to remove highcharts warning

--- src/components/ChartRenderer.jsx
+++ src/components/ChartRenderer.jsx
@@ -13,7 +13,7 @@ function ChartRenderer({
   // Sort the significantData array in descending order of the y value
   // and take the top 4 highest points for the annotations
   const topFourDataPoints = [...significantData]
-    .sort((a, b) => b.y - a.y)
+    .sort((a, b) => b[1] - a[1])
     .slice(0, 4);
 
   const options = {
@@ -77,10 +77,10 @@ function ChartRenderer({
           point: {
             xAxis: 0,
             yAxis: 0,
-            x: point.x,
-            y: point.y,
+            x: point[0],
+            y: point[1],
           },
-          text: point.gene, // Adjust formatting as needed
+          text: point[2], // Adjust formatting as needed
         })),
         labelOptions: {
           backgroundColor: "rgba(255,255,255,0.5)",
@@ -99,7 +99,7 @@ function ChartRenderer({
         // return `${this.point.gene} <br> <b>log2FC:</b> ${this.point.x.toFixed(
         //   2
         // )} <br> <b>-log10(padj):</b> ${this.point.y.toFixed(2)}`;
-        return this.point.gene;
+        return this.point[2];
       },
       hideDelay: 200,
     },
