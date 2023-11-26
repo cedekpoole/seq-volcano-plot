4c84a60c6a487772e3383665aa5b280ea0d23dc2
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-11-26 11:52:57 +0000

    configure vite for npm packaging

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cssInjectedByJsPlugin()],
+  build: {
+    lib: {
+      entry: "src/exports.js", 
+      name: "volcano-plot", 
+      formats: ["es", "umd"], 
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], 
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
